ac4be7bee5e4369d039b46d5a9cfd821c6e77c05
HP <g00433839@atu.ie>
2025-12-31 20:56:54 +0000

    Files added

---
+++ src/test/java/atu/ie/lab8/Lab8ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.lab8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
